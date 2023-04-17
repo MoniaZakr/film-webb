4db5f7fd5fbf68d9cb92c53ad95cf2c299de88c2
Moika Zakrzewska <monikawiniar@gmail.com>
2023-04-17 11:42:16 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <p>hej</p>
+  );
+}
+
+export default App;
---
+++ src/dist/App.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+require("./App.css");
+
+function App() {
+  return p;
+}
+
+var _default = App;
+exports["default"] = _default;
+//# sourceMappingURL=App.dev.js.map
