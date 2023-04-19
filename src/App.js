d34de6dac938df39d8f542ccc8bfe576106a7a72
Moika Zakrzewska <monikawiniar@gmail.com>
2023-04-19 14:40:28 +0200

    Added MowieCard component  and search file.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 
 import './App.css';
+import MowieCard from './components/MowieCard';
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e32129d4';
+
 
 function App() {
   return (
-    <p>hej</p>
+    <>
+      <MowieCard/>
+    </>
   );
 }
 
---
+++ src/components/MowieCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MowieCard = () => {
+  return (
+    <div>MowieCard</div>
+  )
+}
+
+export default MowieCard
