62e7e69bc481faf47a1c0c2ee478dd030947ea4d
Moika Zakrzewska <monikawiniar@gmail.com>
2023-04-20 11:56:21 +0200

    Implemented search input functionality.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-
+import { useEffect } from 'react';
 import './App.css';
 import MowieCard from './components/MowieCard';
+import SearchIcon from "./components/search.svg";
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e32129d4';
 
 
 function App() {
+
+  const searchMovies = async(title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
+  useEffect(() => {
+    searchMovies("Spiderman")
+  }, [])
+
+
   return (
-    <>
-      <MowieCard/>
-    </>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input 
+          type="text"
+          placeholder='Search for movies'
+          value="superman"
+          onChange={() => {}}
+        />
+        <img 
+          src={SearchIcon} 
+          alt="search" 
+          onChange={() => {}} />
+      </div>
+    </div>
   );
 }
 
--- src/dist/App.dev.js
+++ src/dist/App.dev.js
@@ -5,10 +5,47 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
+var _react = require("react");
+
 require("./App.css");
 
+var _MowieCard = _interopRequireDefault(require("./components/MowieCard"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e32129d4';
+
 function App() {
-  return p;
+  var searchMovies = function searchMovies(title) {
+    var response, data;
+    return regeneratorRuntime.async(function searchMovies$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return regeneratorRuntime.awrap(fetch("".concat(API_URL, "&s=").concat(title)));
+
+          case 2:
+            response = _context.sent;
+            _context.next = 5;
+            return regeneratorRuntime.awrap(response.json());
+
+          case 5:
+            data = _context.sent;
+            console.log(data);
+
+          case 7:
+          case "end":
+            return _context.stop();
+        }
+      }
+    });
+  };
+
+  (0, _react.useEffect)(function () {
+    searchMovies("Spiderman");
+  }, []);
+  return div.app > h1;
 }
 
 var _default = App;
