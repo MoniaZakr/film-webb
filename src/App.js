f977366cb262d4d51de48ba2bd1386949f2f966d
Moika Zakrzewska <monikawiniar@gmail.com>
2023-04-22 09:26:23 +0200

    Improved application performance.

--- src/App.js
+++ src/App.js
@@ -4,48 +4,44 @@ import MovieCard from './components/MovieCard';
 import SearchIcon from "./components/search.svg";
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e32129d4';
-const movie1 = {
-  "Title": "Spiderman",
-  "Year": "1990",
-  "imdbID": "tt0100669",
-  "Type": "movie",
-  "Poster": "N/A"
-}
+
 
 
 function App() {
-  const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+  
 
-  const searchMovies = async(title) => {
+  useEffect(() => {
+    searchMovies("Batman")
+  }, [])
+
+  const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
     
     setMovies(data.Search);
   }
 
-  useEffect(() => {
-    searchMovies("Spiderman")
-  }, [])
-
+  
 
   return (
     <div className="app">
       <h1>MovieLand</h1>
       <div className="search">
         <input 
-          type="text"
           placeholder='Search for movies'
-          value="superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img 
           src={SearchIcon} 
           alt="search" 
-          onChange={() => {}} />
+          onClick={() => searchMovies(searchTerm)} />
       </div>
       {movies?.length > 0 ? (
         <div className="container">
-          {movies.map((movie) => (
+          {movies.map((movie, id) => (
             <MovieCard movie={movie}/>
           ))}
         </div>
--- src/dist/App.dev.js
+++ src/dist/App.dev.js
@@ -1,53 +1,14 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-
 var _react = require("react");
 
 require("./App.css");
 
-var _MowieCard = _interopRequireDefault(require("./components/MowieCard"));
+var _MovieCard = _interopRequireDefault(require("./components/MovieCard"));
+
+var _search = _interopRequireDefault(require("./components/search.svg"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e32129d4';
-
-function App() {
-  var searchMovies = function searchMovies(title) {
-    var response, data;
-    return regeneratorRuntime.async(function searchMovies$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            _context.next = 2;
-            return regeneratorRuntime.awrap(fetch("".concat(API_URL, "&s=").concat(title)));
-
-          case 2:
-            response = _context.sent;
-            _context.next = 5;
-            return regeneratorRuntime.awrap(response.json());
-
-          case 5:
-            data = _context.sent;
-            console.log(data);
-
-          case 7:
-          case "end":
-            return _context.stop();
-        }
-      }
-    });
-  };
-
-  (0, _react.useEffect)(function () {
-    searchMovies("Spiderman");
-  }, []);
-  return div.app > h1;
-}
-
-var _default = App;
-exports["default"] = _default;
 //# sourceMappingURL=App.dev.js.map
